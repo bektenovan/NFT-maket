363c726194dae305bab1ecf33cc4f0420d6057ca
bektenovan <bektenovan.01@gmail.com>
2023-03-19 16:16:17 +0600

    marketplace-desktop

---
+++ src/Components/Marketplace.jsx
@@ -0,0 +1,263 @@
+import React from "react";
+const Marketplace = () => {
+    return (
+        <div className="headline-marketplace">
+            <div className="headline-section">
+                <div className="market-text">
+                    <h1>Browse Marketplace</h1>
+                    <p>Browse through more than 50k NFTs on the NFT Marketplace.</p>
+                </div>
+                <div className="search-table">
+                    <div className="table-text">
+                        <input type="text" placeholder="Search your favourite NFTs" name="" id="" />
+                    </div>
+                    <img src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/magnifyingglass@2x.svg" alt="" />
+                </div>
+            </div>
+            <div className="marketplace-section">
+                <div className="line-frame">
+                    <img src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/line-2-3@1x.svg" alt="" />
+                    <div className="column-frame">
+                        <div className="column-tabs">
+                            <div className="column-tabs2">
+                                <div className="tabs-nft">Nfts</div>
+                                <div className="tabs-num">
+                                    <div className="tabs-302">203</div>
+                                </div>
+                            </div>
+
+                            <div className="tabs">
+                                <div className="tabs-col">
+                                    Collections
+                                </div>
+                                <div className="tabs-col2">
+                                    <div className="tabs-67">67</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="nft-card-section">
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3 >Magic Mushroom 0325</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-2@2x.png" alt="" /></li>
+                                <li >Shroomie</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-32@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Happy Robot 032</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-54@2x.png" alt="" /></li>
+                                <li >BeKind2Robots</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-33@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Happy Robot 024</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-54@2x.png" alt="" /></li>
+                                <li >BeKind2Robots</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-34@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Designer Bear</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder@2x.png" alt="" /></li>
+                                <li >Mr Fox</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-35@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Colorful Dog 0356</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-57@2x.png" alt="" /></li>
+                                <li >Keepitreal</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-36@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Dancing Robot 0312</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-58@2x.png" alt="" /></li>
+                                <li >Robotica</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-37@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Cherry Blossom Girl 035</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-59@2x.png" alt="" /></li>
+                                <li >MoonDancer</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-38@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Space Travel</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-20@2x.png" alt="" /></li>
+                                <li >NebulaKid</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-2@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Sunset Dimension</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-33@2x.png" alt="" /></li>
+                                <li >Animakid</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-71@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Desert Walk</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-120@2x.png" alt="" /></li>
+                                <li >Catch 22</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-72@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>IceCream Ape 0324</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-121@2x.png" alt="" /></li>
+                                <li >Ice Ape Club</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="more-card" >
+                        <img width="330px" height="296px" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/image-placeholder-35@2x.png" alt="" />
+                        <div class="more-body">
+                            <h3>Colorful Dog 0344</h3>
+                            <ul className="more__ava">
+                                <li><img width='24' height='24' class="scale" src="https://cdn.animaapp.com/projects/63aaf7e2426e9824f0350c11/releases/63aaf8f2426e9824f0350c13/img/avatar-placeholder-72@2x.png" alt="" /></li>
+                                <li >PuppyPower</li>
+                            </ul>
+                            <ul className="more-info">
+                                <li>Price</li>
+                                <li>Highest Bid</li>
+                            </ul>
+                            <ul className="eth">
+                                <li>1.63 ETH</li>
+                                <li>0.33 wETH</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Marketplace;
--- src/Routing.js
+++ src/Routing.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './Components/HomePage'
 import CreateAccount from './Components/CreateAccount';
 import ConnectWallet from './Components/ConnectWallet';
+import Marketplace from './Components/Marketplace';
 
 const Routing = () => {
     return(
@@ -10,6 +11,9 @@ const Routing = () => {
     <Route path='/' element={<HomePage/>}/>
     <Route path='/signUp' element={<CreateAccount/>}/>
     <Route path='/wallet' element={<ConnectWallet/>}/>
+    <Route path='/marketplace' element={<Marketplace/>}/>
+
+
 
 </Routes>
     );
